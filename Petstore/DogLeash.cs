bd8d2558bc1accd8839c6139fc29d6930bed0e3a
knunn553 <knunn553@outlook.com>
2024-07-26 11:36:35 -0400

    Added functionality for Pet Store application. Introduced concepts such as inheritance between the Product class and the two base classes of CatFood and DogLeash. Further studies internal access modifier. If the parent class has the internal access modifier and we inherit from a child class, we don't have to specify its access modifier, its implied. Still has to be within the same assembly, or .dll. In the logic, implemented while loop to enable to program to run while the userinput did not equal exit, which exits the program. Demonstrated encapsulation with property getter and setters within each class. Demonstrated ability to instantiate objects from classes and add values to the properties of each class. In addition, began playing with try catch and exception handling based classes such as Exception and ArgumentException in .NET as well as how to display a custom exception handler.

---
+++ Petstore/DogLeash.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petstore
+{
+    internal class DogLeash : Product
+    {
+        public int LengthInches { get; set; }
+        public string Material { get; set; }
+    }
+}
---
+++ Petstore/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Xml.Linq;
+
+namespace Petstore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = string.Empty;
+            Console.WriteLine("Press 1 to add product");
+            Console.WriteLine("Type 'exit' to exit the program after the completion of inserting a new product");
+
+            while (userInput.ToLower() != "exit")
+            {
+                try
+                {
+                    userInput = Console.ReadLine();
+                    // Let's implement some expection handling in this application
+                    // If user input is different from what we would expect, lets use try catch for practice
+
+                    // Need to add functionality to ask for how many products you want to enter, then create a for loop from that point on.
+                    if(userInput != "1" && userInput.ToLower() != "exit")
+                    {
+                        throw new Exception("Follow directions shithead. Enter 1 to add a product or enter 'exit' to end the program.");
+                    }
+
+                    if (userInput == "1")
+                    {
+                        var product = new DogLeash();
+                        Console.WriteLine("Enter name of product:");
+                        product.Name = Console.ReadLine();
+                        string name = product.Name;
+                        Console.WriteLine("Enter description of product:");
+                        product.Description = Console.ReadLine();
+                        string description = product.Description;
+                        Console.WriteLine("Enter material of product:");
+                        product.Material = Console.ReadLine();
+                        string material = product.Material;
+                        Console.WriteLine();
+                        Console.WriteLine($"Successfully Inserted:\nProduct Name: {name} \nProduct Description: {description} \nProduct Material: {material}");
+                        Console.WriteLine();
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+                
+            } 
+        }
+    }
+}
