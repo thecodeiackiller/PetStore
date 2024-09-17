87102fb0ed2a97dc65f2955dcae750ae2d8a2caa
knunn553 <knunn553@outlook.com>
2024-09-16 20:02:20 -0400

    Per PetStore 7, added dependency injection capabilities to the application creating a method returning an IServiceProvider. We are creating services through the dependency injection container.Might need to go back and review the implementation, but for now, that's all she wrote.

--- Petstore/Program.cs
+++ Petstore/Program.cs
@@ -3,50 +3,47 @@ using System.Xml.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using Petstore.UILogic;
 using PriceChanges.Extensions;
-using Microsoft.Extensions.DependencyInjection; // This is correct for AddTransient
+
 using Microsoft.Extensions.Hosting;
 
 namespace Petstore
 {
     public class Program
     {
-        // Assigning the result of CreateServiceCollection() to a variable
-        //static IServiceProvider service = CreateServiceCollection();
+        
+        static IServiceProvider service = CreateServiceCollection();
 
         static void Main(string[] args)
         {
-            
-            //var productLogic = service.GetService<IProductLogic>();
-            
-            // To access the members of the UserInput class, instead of the following, we can use DI.
 
-            UserInput userInput = new UserInput();
-            userInput.ListUserInputOptions();
-            userInput.GetUserInput();
-            userInput.ExecuteUserInput();
-
-            // Dependency Injection 
-            // Create the service provider - this just allows us to handle our service resolutions
-            var serviceProvider = CreateServiceCollection();
+            var productlogic = service.GetService<IProductLogic>();
             // Resolving our service
-
+            var userInput = service.GetService<IUILogic>();
             // Calling methods from UserInput class (i.e. using our service through DI)
+            if (userInput != null)
+            {
+                userInput.ListUserInputOptions();
+                userInput.GetUserInput();
+                userInput.ExecuteUserInput();
+            }
+            return;
+            
 
         }
 
-        
-        // DI is typically done in the program.cs or startup.cs classes
+
         // DI is typically done through IServiceCollection
         // Different from IServiceProvider, IServiceCollection can't be passed into a class constructor
-        //static IServiceProvider CreateServiceCollection()
-        //{
-        //    var newServiceCollection = new ServiceCollection();
-            
-        //    // Configuring Services here
-        //    newServiceCollection.AddTransient<IProductLogic, ProductLogic>();
-            
+        static IServiceProvider CreateServiceCollection()
+        {
+            var newServiceCollection = new ServiceCollection();
 
-        //    return newServiceCollection.BuildServiceProvider();
-        //}
+            // Configuring Services here
+            newServiceCollection.AddTransient<IProductLogic, ProductLogic>();
+            newServiceCollection.AddTransient<IUILogic, UserInput>();
+
+
+            return newServiceCollection.BuildServiceProvider();
+        }
     }
 }
